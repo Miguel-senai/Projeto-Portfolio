c2f2a97df64e338f8f673a534dce2335451b1e86
Miguel-senai <miguel.feitosa@aluno.senai.br>
2025-06-06 17:18:12 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,8 @@
+// typing js
+
+var typed = new Typed('#escrita', {
+    strings: ['Estudante de Frontend', 'Estudante de Backend', 'Estudante de Programação'],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+});
